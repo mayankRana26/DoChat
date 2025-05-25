b5b8c80df0edf4591c9d6158021387dc704b4cc5
mayankRana26 <ranamayank987@gmail.com>
2025-05-25 16:46:26 +0530

    ready to production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,9 @@ export default defineConfig({
     port:3000,
     proxy: {
       '/api': {
-        target:'http://localhost:5000'
+        target:'http://localhost:5000',
+        changeOrigin: true,
+        secure: false, 
       }
     }
   }
