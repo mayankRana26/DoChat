d49e160eb912792fe98af8cea84d84732266d393
mayankRana26 <ranamayank987@gmail.com>
2025-06-01 22:13:08 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ const __dirname = path.resolve();
 
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables.
 const PORT = process.env.PORT || 5000;
+const BACKEND_URL = "https://dochat-3.onrender.com";
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -36,9 +37,9 @@ app.get("/", (req, res) => {
 		error: "false",
 	})
 })
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes);
+app.use(`${BACKEND_URL}/api/auth`, authRoutes);
+app.use(`${BACKEND_URL}/api/messages`, messageRoutes);
+app.use(`${BACKEND_URL}/api/users`, userRoutes);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
