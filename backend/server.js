979fef8805286bbe07e9f77a48b0174bd77707ea
mayankRana26 <ranamayank987@gmail.com>
2025-06-01 17:48:53 +0530

    after using /splat

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const __dirname = path.resolve();
 
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables.
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -25,6 +25,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("/*splat", (req, res) => {
+ res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
+});
+
 
 server.listen(PORT, () => {
 	connectToMongoDB();
